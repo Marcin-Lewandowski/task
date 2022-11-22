5cc17b1b6cdbd979a726fd55623fda9afb2244cd
Marcin Lewandowski <telfordmarcin@gmail.com>
2022-11-22 17:51:06 +0000

    Pozdowienia z krainy deszczowców. Karambaaa

---
+++ pozdro.py
@@ -0,0 +1,3 @@
+# Cześć Paweł. Powiem Ci, że nie spodziewałem się jak mocno będę sfrustrowany ucząc się podstaw gita :/
+# Jestem nieco przybity ale cierpliwie brnę dalej w naukę. Mam teraz urlop więc mogę sporo czasu przeznaczyć na mocowanie się z tematem gita i ogólnie kursem.
+# Pozdrawiam z krainy deszczowców. Karamba ;)
