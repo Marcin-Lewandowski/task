031d91795cc9209374ff91879825963769fb6b90
Marcin Lewandowski <telfordmarcin@gmail.com>
2023-01-21 17:06:40 +0000

    Flask - zadanie

---
+++ Zadanie_flask/flask_2.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask import request, redirect
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/me')
+def me():
+	return render_template('index.html')
+
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'GET':
+       return render_template("contact.html")
+    elif request.method == 'POST':
+        print(request.form)
+        return redirect("/")
